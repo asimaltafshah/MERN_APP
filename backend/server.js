174878d5bda47e3c55c278bb95b893a80942b08b
Asim Altaf Shah <asimaltafshah@gmail.com>
2022-08-23 14:17:17 +0500

    Connected to Mongodb using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 //express app
@@ -15,9 +16,16 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/workouts", workoutRoutes);
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log(`listening to port ${process.env.PORT}`);
-});
 
-process.env;
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to db & listening to port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
